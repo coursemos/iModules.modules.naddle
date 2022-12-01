a3d2b5531da3b3cdaa3792ae7fb27b4172932ce2
arzzcom <arzz@arzz.com>
2022-12-01 14:03:29 +0900

    코스모스 공식 홈페이지 모듈 추가

---
+++ coursemos/Coursemos.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 이 파일은 아이모듈 코스모스-나들모듈 일부입니다. (https://www.imodules.io)
+ *
+ * 코스모스-나들모듈 클래스를 정의한다.
+ *
+ * @file /modules/coursemos/naddle/Naddle.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2022. 11. 23.
+ */
+namespace modules\naddle\coursemos;
+class Coursemos extends \Module
+{
+}
